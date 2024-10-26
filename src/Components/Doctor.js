1267dd2b87252e726ee66eb970af3ad0ad0f31f4
Pranjal095 <pranjal.prajapati35@gmail.com>
2024-10-27 02:58:53 +0530

    Prescription download functionality added

--- src/Components/Doctor.js
+++ src/Components/Doctor.js
@@ -3,12 +3,74 @@ import '../Styles/Doctor.css';
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import io from "socket.io-client";
+import jsPDF from 'jspdf';
+import 'jspdf-autotable';
 
 const Doctor = () => {
   const [patients, setPatients] = useState([]);
   const [prescribedPatients, setPrescribedPatients] = useState([]);
   const [error, setError] = useState("");
   const navigate = useNavigate();
+
+  const generatePDF = (index) => {
+    const patient = prescribedPatients[index];
+    const doc = new jsPDF('p', 'mm', 'a4');
+
+    doc.setFontSize(17);
+    doc.setFont("helvetica", "bold");
+    doc.text('Telangana PHC', 14, 20);
+    doc.setLineWidth(0.4);
+    doc.line(14, 24, 196, 24);
+    doc.setFontSize(14);
+    doc.setFont("helvetica", "bold");
+    doc.text('Patient Prescription', 14, 30);
+    doc.setLineWidth(0.4);
+    doc.line(14, 34, 196, 34);
+    doc.setFontSize(12);
+    doc.setFont("helvetica", "normal");
+    const details = [
+        `Name: ${patient.name}`,
+        `Age: ${patient.age}`,
+        `Gender: ${patient.gender}`,
+        `Aadhar Number: ${patient.aadhar_number}`
+    ];
+
+    details.forEach((detail, i) => {
+        doc.text(detail, 14, 40+i*10);
+    });
+
+    const complaintsString = patient.complaint.replace(/[{}"]/g, '').trim();
+    const complaints = complaintsString.split(',').map(item => item.trim());
+    doc.setFontSize(12);
+    doc.setFont("helvetica", "bold");
+    doc.text('Complaint(s):', 14, 80);
+    doc.setFont("helvetica", "normal");
+    complaints.forEach((complaint, i) => {
+        doc.text(`- ${complaint}`, 20, 90+i*10);
+    });
+    const medicinesString = patient.medicines_prescribed.replace(/[{}"]/g, '').trim();
+    const medicines = medicinesString.split(',').map(item => item.trim());
+    const startY = 92 + complaints.length * 10;
+    doc.setFontSize(12);
+    doc.setFont("helvetica", "bold");
+    doc.text('Medicines Prescribed:', 14, startY);
+    doc.setFont("helvetica", "normal");
+
+    medicines.forEach((medicine, i) => {
+        doc.text(`- ${medicine}`, 20, startY+10+i*10);
+    });
+
+    const listEndY = startY+6+medicines.length*10;
+    doc.setLineWidth(0.4);
+    doc.line(14, listEndY+10, 196, listEndY+10);
+    doc.setFontSize(12);
+    doc.setFont("helvetica", "italic");
+    doc.text('Date: '+new Date().toLocaleDateString(), 14, listEndY+25);
+
+    doc.save(`Prescription-${patient.name}.pdf`);
+};
+
+
 
   useEffect(() => {
     const socket = io("http://localhost:8001");
@@ -86,13 +148,13 @@ const Doctor = () => {
             )}
           </div>
 
-          <h2 className="section-title">Prescribed Patients</h2>
+          <h2 className="section-title">Download Prescription</h2>
           <div className="patients">
             {prescribedPatients.length === 0 ? (
               <p className="message">No prescribed patients available.</p>
             ) : (
-              prescribedPatients.map((patient) => (
-                <div key={patient.id} className="patient-card">
+              prescribedPatients.map((patient,index) => (
+                <div key={patient.id} className="patient-card" onClick={() => generatePDF(index)}>
                   <h3 className="patient-name">{patient.name}</h3>
                   <p className="patient-details">Age: {patient.age}</p>
                   <p className="patient-details">Gender: {patient.gender}</p>
